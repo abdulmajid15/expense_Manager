3a961683c00b92a5e694b74a381aba251a09a27e
Abdul Majid <abromajid18@gmail.com>
2021-12-25 22:21:47 +0500

    fix title

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Personal Expenses',
+      title: 'Expense Manager',
       theme: ThemeData(
           primarySwatch: Colors.teal,
           accentColor: Colors.amber,
